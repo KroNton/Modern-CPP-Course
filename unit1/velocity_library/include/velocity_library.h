cbb1e63a29b75eab925d4ae8345796e52b491700
KroNton <kyrlosfekry@gmail.com>
2023-08-30 22:21:40 +0300

    create velocity liberary pkg

---
+++ unit1/velocity_library/include/velocity_library.h
@@ -0,0 +1,8 @@
+#ifndef VELOCITY_LIBRARY_H
+#define VELOCITY_LIBRARY_H
+
+#include <ros/ros.h>
+
+void display_vel(float linear, float angular);
+
+#endif
---
+++ unit1/velocity_library/src/velocity_library.cpp
@@ -0,0 +1,9 @@
+#include "../include/velocity_library.h"
+
+void display_vel(float linear, float angular) {
+     
+    float linear_vel = linear;
+    float angular_vel = angular;
+    std::cout << "Linear velocity: " << linear_vel << " Angular velocity: " << angular_vel << std::endl;
+     
+}
