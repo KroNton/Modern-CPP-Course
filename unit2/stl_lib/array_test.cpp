00927e000992ec83101588b5b6565803309ac3da
KroNton <kyrlosfekry@gmail.com>
2023-08-30 21:16:32 +0300

    add odom sum pkg

---
+++ unit2/stl_lib/array_test.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+using namespace std;
+  
+int main()
+{
+    int arr[2]={42, 100};
+  
+    cout << arr[1] << " ";
+    arr[1]= 80;
+    cout << arr[1] << " ";
+  
+    return 0;
+}
