d2f174b0f1a4ac395ba2d4f89830a8524483b824
KroNton <kyrlosfekry@gmail.com>
2023-09-11 02:41:37 +0300

    add robot manager node

---
+++ unit3/my_robot_manager/src/robot_manager_node.cpp
@@ -0,0 +1,13 @@
+#include <ros/ros.h>
+#include "my_robot_manager/robot_manager.h"
+
+
+
+int main(int argc, char  **argv)
+{
+    ros::init(argc, argv, "robot_manager");
+    ros::NodeHandle nh;
+    RobotManager robot_manger(&nh);
+    ros::spin();
+    return 0;
+}
