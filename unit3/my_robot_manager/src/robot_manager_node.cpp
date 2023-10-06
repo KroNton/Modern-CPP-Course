3b888ba7470e8548e4c0ffd4ef415400375b695a
KroNton <kyrlosfekry@gmail.com>
2023-10-06 15:51:36 +0300

    use libarary to move the robot and get poses

--- unit3/my_robot_manager/src/robot_manager_node.cpp
+++ unit3/my_robot_manager/src/robot_manager_node.cpp
@@ -1,6 +1,12 @@
 #include <ros/ros.h>
 #include "my_robot_manager/robot_manager.h"
 
+struct robot_position{
+
+float x;
+float y;
+
+};
 
 
 int main(int argc, char  **argv)
@@ -8,12 +14,29 @@ int main(int argc, char  **argv)
     ros::init(argc, argv, "robot_manager");
     ros::NodeHandle nh;
     RobotManager rm1(&nh,"turtlebot3_1","/cmd_vel","/odom");
-
-    float robot1_x=rm1.get_coords(1);
-    float robot1_y=rm1.get_coords(2);
-    ROS_INFO("%s Position (x,y): %lf , %lf",rm1.get_robot_name().c_str(),robot1_x , robot1_y);
-
+    robot_position robot1_pose;
 
     
+    
+    rm1.move_forward(2);
+
+    robot1_pose.x=rm1.get_coords(1);
+    robot1_pose.y=rm1.get_coords(2);
+
+    std::cout<< "Current position of the first robot"<<std::endl;
+    ROS_INFO("%s Position (x,y): %lf , %lf",rm1.get_robot_name().c_str(),robot1_pose.x , robot1_pose.y);
+    
+    rm1.move_backward(2);
+
+    RobotManager robot_copy=rm1;
+    
+    
+    robot1_pose.x=robot_copy.get_coords(1);
+    robot1_pose.y=robot_copy.get_coords(2);
+    std::cout<< "Final position of the first robot"<<std::endl;
+    ROS_INFO("%s Position (x,y): %lf , %lf",rm1.get_robot_name().c_str(),robot1_pose.x , robot1_pose.y);
+
+    
+  
     return 0;
 }
