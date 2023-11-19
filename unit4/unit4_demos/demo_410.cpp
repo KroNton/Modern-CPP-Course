d7f896fb14b52bbeeadac3018a51e14e44f23f17
KroNton <kyrlosfekry@gmail.com>
2023-11-19 02:19:13 +0200

    Polymorphism example

---
+++ unit4/unit4_demos/demo_410.cpp
@@ -0,0 +1,16 @@
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    float r = 4.23;
+    int k =2;
+    char a='a';
+    int sum = k + r;
+    cout<< "sum :"<< sum <<" Here r is typecasted to be an integer\n ";
+    sum = sum + a;
+    cout<< "sum :"<< sum <<" Here a is typecasted to be an integer, it's ASCII value is taken\n ";
+
+    return 0;
+}
