2be9d49a0894d0fc2f832f45037863eef2980351
KroNton <kyrlosfekry@gmail.com>
2024-11-28 01:41:55 +0200

    examples on derefrence

---
+++ selected-topics/pointers/3_dereference.cpp
@@ -0,0 +1,18 @@
+// @file dereference.cpp
+// g++ -std=c++17 3_dereference.cpp -o prog
+#include <iostream>
+
+int main(){
+    // Initialize integer
+    int x= 7;
+    // Pointer assigned to store address of x
+    int* px= &(x);
+    // Print out for our understanding
+    std::cout << "x value        : " <<  x  << std::endl;
+    std::cout << "x address      : " << &x  << std::endl;
+    std::cout << "px points to   : " << px  << std::endl;
+    std::cout << "px dereferenced: " << *px << std::endl;
+        
+    return 0;
+}
+
