4b11c69ab25056ec4fe912b11a7f4c54b5b65388
KroNton <kyrlosfekry@gmail.com>
2024-11-28 00:01:16 +0200

    add example 4

---
+++ selected-topics/lambda-expressions/examples/lambda_expression_ex4.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+/*
+lambda expression
+an unnamed function object capable of capturing variables in scope
+
+Basic syntax: [captures]  (params) {body}
+*/
+int global =-3;
+int main(int argc, char const *argv[])
+{
+
+    std::vector<int> my_vect{1, 3, 4, 7, 8, 2};
+
+    // using lambda by storing function object in a varible
+    int last_Result = -1;
+    int last_Result2 = -2;
+/*
+can use " = " or "last_Result(without &)" for read only it const can't be changed
+
+can use  "last_Result, last_Result2" or "=" with mutable as it will not give error if u assign value but 
+nothing will be changed as variables already pass by value 
+*/
+    auto print_v_1 = [=](int element) 
+    {   
+        global=500; // global variable can be accesed and changed in any part of the code 
+        std::cout << last_Result << " "; 
+        std::cout << element << " ";};
+
+    auto print_v_2 = [last_Result, last_Result2] (int element) mutable
+    {   last_Result= element;
+        last_Result2= element;
+        std::cout << element << " "; };
+
+    std::cout << "lambda function output:" << std::endl;
+
+    std::for_each(my_vect.begin(), my_vect.end(), print_v_1);
+
+    std::cout << std::endl;
+
+    std::cout << std::endl;
+    std::cout << " last result:" << last_Result << std::endl;
+    std::cout << " last result2:" << last_Result2 << std::endl;
+    std::cout << " global: " << global << std::endl;
+
+    return 0;
+}
